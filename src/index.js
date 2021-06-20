c0cae03a38c42a6a56dc7e0dd8d166f3106cc224
Alex Anastasopol <anastasopolalex@gmail.com>
2021-06-20 15:58:43 +0300

    tell me a joke button - logs to cosole dad joke from icanhazdadjoke.com

--- src/index.js
+++ src/index.js
@@ -3,28 +3,20 @@ import ReactDOM from "react-dom";
 
 import "./App.css";
 
-// class Add extends React.Component{
-// render() {
-//   return <h1>{this.props.a+this.props.b}</h1>;
-// }}
+function App(props) {
+    const onTellJoke = ()=>{
 
-const Layout = (props) => (
-    <React.Fragment>
-        <header>My header</header>
-        <main>{props.children}</main>
-        <footer>My Footer</footer>
-    </React.Fragment>
+        fetch('https://icanhazdadjoke.com/',
+            {
+                method:'GET',
+                headers:{
+                    Accept:'application/json'
+                }
+            }).then(response => response.json())
+            .then(json1 => console.log('Click!!! '+json1.joke));
+    }
 
-);
-
-function App() {
-    return (
-        <Layout>
-            <p>page 1 body content</p>
-
-            <p>more page 1 body content</p>
-        </Layout>
-    );
+    return <button onClick={onTellJoke}>Tell me a joke</button>;
 }
 
 const rootElement = document.getElementById("root");
