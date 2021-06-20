93d97373579344a5921db0c01e9b61e57bd94bbb
Alex Anastasopol <anastasopolalex@gmail.com>
2021-06-20 16:32:28 +0300

    a

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ class App extends React.Component {
     }
 
     render() {
-        console.log('--- RENDER ---');
+        console.log('--- RENDER -----');
 
         return (
             <div>
