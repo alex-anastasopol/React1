f341b6b46f79621b298950a83fe3557080495981
Alex Anastasopol <anastasopolalex@gmail.com>
2021-06-20 16:26:26 +0300

    shows one joke on screen per button press

--- src/index.js
+++ src/index.js
@@ -3,21 +3,44 @@ import ReactDOM from "react-dom";
 
 import "./App.css";
 
-function App(props) {
-    const onTellJoke = ()=>{
+class App extends React.Component {
+    joke = null;
 
-        fetch('https://icanhazdadjoke.com/',
-            {
-                method:'GET',
-                headers:{
-                    Accept:'application/json'
-                }
-            }).then(response => response.json())
-            .then(json1 => console.log('Click!!! '+json1.joke));
+    constructor() {
+        super();
+        this.state = {
+            joke: null
+        }
+        this.onTellJoke = this.onTellJoke.bind(this);
     }
 
-    return <button onClick={onTellJoke}>Tell me a joke</button>;
+    onTellJoke() {
+        fetch('https://icanhazdadjoke.com',
+            {
+                METHOD: 'GET',
+                headers: {
+                    Accept: 'application/json'
+                }
+            }
+        ).then(response => response.json())
+            .then(json1 => {
+             this.setState({
+                 joke:json1.joke
+             });
+            });
+    }
+
+    render() {
+        console.log('--- RENDER ---');
+
+        return (
+            <div>
+                <button onClick={this.onTellJoke}>Tell me a joke</button>
+                <p>{this.state.joke}</p>
+            </div>
+        );
+    }
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App version="1.0"/>, rootElement);
